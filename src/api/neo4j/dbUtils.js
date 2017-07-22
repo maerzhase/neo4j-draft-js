6f8c3f507f6c41040cb1f4909d836adc289e0a77
maerzhase <markus@lili16.de>
2017-07-22 18:12:07 +0200

    init db with env variables

--- src/api/neo4j/dbUtils.js
+++ src/api/neo4j/dbUtils.js
@@ -1,9 +1,7 @@
 import { v1 as neo4j } from 'neo4j-driver';
+import env from '../../../dev/env';
 
-const uri = 'bolt://127.0.0.1:7687';
-const user = 'neo4j';
-const password = 'draft-js';
-const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+const driver = neo4j.driver(process.env.NEO4J_HOST, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD));
 
 export const getSession = () => driver.session();
 
